ae8b89f3de5cf700a0ab0c2bfa57574a9c876222
Isabela Torres <isabelatorres.torres@gmail.com>
2022-06-03 17:53:53 -0300

    Laravel até aula #23

--- Aula_20-05/example-app/app/Http/Controllers/EventController.php
+++ Aula_20-05/example-app/app/Http/Controllers/EventController.php
@@ -7,28 +7,63 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    public function index(){
+  public function index() {
+
+    $search = request('search');
+
+    if ($search) { 
+      $events = Event::where([
+        ['title', 'like', '%'.$search.'%']
+      ])->get();
+    }else {
       $events = Event::all();
-        return view('welcome', 
-            ['events' => $events]);
     }
 
-    public function create() {
-      return view('/events/create');
+    return view(
+      'welcome', ['events' => $events, 'search' => $search]
+    );
   }
 
-  public function store(Request $request){
-
-      $event = new Event;
-
-      $event->title = $request->title;
-      $event->city = $request->city;
-      $event->private = $request->private;
-      $event->description = $request->description;
-
-      $event->save();
-
-      return redirect('/')->with('msg', 'Evento criado com sucesso!');
+  public function create(){
+    return view('/events/create');
   }
 
+  public function store(Request $request) {
+
+    $event = new Event;
+
+    $event->title = $request->title;
+    $event->date = $request->date;
+    $event->city = $request->city;
+    $event->private = $request->private;
+    $event->description = $request->description;
+    $event->image = $request->image;
+    $event->items = $request->items;
+
+    if ($request->hasFile('image') && $request->file('image')->isValid()) {
+
+      $requestImage = $request->image;
+      $extension = $requestImage->extension();
+
+      $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+      $requestImage->move(public_path('img/events'), $imageName);
+
+      $event->image = $imageName;
+    }
+
+    $user = auth()->user();
+    $user->user_id = $user->id;
+
+    $event->save();
+
+    return redirect('/')->with('msg', 'Evento criado com sucesso!');
+  }
+
+  public function show($id)
+  {
+    $event = Event::findOrFail($id);
+
+    return view('/events/show', ['event' => $event]);
+  }
 }
